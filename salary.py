6858d7b98c700c37ac319ee0a03678d732b3d309
Adrian <adriannugrohobasuki@gmail.com>
2023-12-04 18:58:46 +0700

    Update salary.py

--- salary.py
+++ salary.py
@@ -2,36 +2,35 @@ userData = {}
 salary = 3800000
 hourlyOvertimeRate = 110000
 
-while True: 
-    numberOfOvertime = (input("Enter the number of overtime: "))
-    while not (numberOfOvertime.isdigit()) or not (int(numberOfOvertime) <= 20 and int(numberOfOvertime) >= 0):
-                print(f"Please enter a number in range of 0 and 20")
-                numberOfOvertime = (input("Enter the number of overtime: "))
-    numberOfOvertime = int(numberOfOvertime)
+numberOfOvertime = (input("Enter the number of overtime: "))
+while not (numberOfOvertime.isdigit()) or not (int(numberOfOvertime) <= 20 and int(numberOfOvertime) >= 0):
+        print(f"Please enter a number in range of 0 and 20")
+        numberOfOvertime = (input("Enter the number of overtime: "))
+numberOfOvertime = int(numberOfOvertime)
 
-    levelOfAllowance = (input("Enter level of allowance: "))
-    while levelOfAllowance not in ["1","2","3"]:
-        print(f"Enter level of allowance (1 to 3):")
+levelOfAllowance = (input("Enter level of allowance: "))
+while levelOfAllowance not in ["1","2","3"]:
+        print(f"Enter level of allowance (1 to 3)")
         levelOfAllowance = (input("Enter level of allowance: "))
-    levelOfAllowance = int(levelOfAllowance)
+levelOfAllowance = int(levelOfAllowance)
 
-    if levelOfAllowance == 1:
-            salary += numberOfOvertime * hourlyOvertimeRate
-            salary += 3800000 * 0.05
-            print("-----------------------------------------")
-            print(f"The Total Salary is: {int(salary)}")
-            break
+if levelOfAllowance == 1:
+        salary += numberOfOvertime * hourlyOvertimeRate
+        salary += 3800000 * 0.05
+        print("-----------------------------------------")
+        print(f"The Total Salary is: {int(salary)}")
 
-    elif levelOfAllowance == 2:
-            salary += numberOfOvertime * hourlyOvertimeRate
-            salary += 3800000 * 0.1
-            print("-----------------------------------------")
-            print(f"The Total Salary is: {int(salary)}")
-            break
+elif levelOfAllowance == 2:
+        salary += numberOfOvertime * hourlyOvertimeRate
+        salary += 3800000 * 0.1
+        print("-----------------------------------------")
+        print(f"The Total Salary is: {int(salary)}")
 
-    elif levelOfAllowance == 3:
-            salary += numberOfOvertime * hourlyOvertimeRate
-            salary += 3800000 * 0.15
-            print("-----------------------------------------")
-            print(f"The Total Salary is: {int(salary)}")
-            break
+elif levelOfAllowance == 3:
+        salary += numberOfOvertime * hourlyOvertimeRate
+        salary += 3800000 * 0.15
+        print("-----------------------------------------")
+        print(f"The Total Salary is: {int(salary)}")
+
+
+
